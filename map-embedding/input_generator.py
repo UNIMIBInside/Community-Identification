cf8b881750eea1b5e9b41a74c87693a09969360d
Coich <coich1996@gmail.com>
2021-01-13 19:23:37 +0100

    Aggiornati input rete neurale

---
+++ map-embedding/input_generator.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.applications.resnet50 import preprocess_input
+
+
+def generate_data(datagenerator, metadata_dataframe, image_path, columns, \
+                                                    batch_size, target_size_1, target_size_2):
+    data_generator = datagenerator.flow_from_dataframe(
+        dataframe=metadata_dataframe,
+        directory=image_path,
+        x_col="map_tiles",
+        y_col=columns,
+        subset="training",
+        batch_size=batch_size,
+        seed=42,
+        shuffle=True,
+        class_mode="raw",
+        target_size=(target_size_1, target_size_2))
+    return data_generator
+
+def generate_data_multitasking(datagenerator, metadata_dataframe, image_path, columns, \
+                                                    batch_size, target_size_1, target_size_2):
+    data_generator = datagenerator.flow_from_dataframe(
+        dataframe=metadata_dataframe,
+        directory=image_path,
+        x_col="map_tiles",
+        y_col=columns,
+        subset="training",
+        batch_size=batch_size,
+        seed=42,
+        shuffle=True,
+        class_mode="raw",
+        target_size=(target_size_1, target_size_2))
+    total_images = data_generator.n
+    steps = int(np.ceil(total_images/batch_size))
+
+    x , y = [], []
+    for _ in range(steps):
+        a , b = data_generator.next()
+
+        #a = tf.convert_to_tensor(a)
+        #b = tf.convert_to_tensor(b)
+        a = np.array(a, dtype=np.uint8)
+        b = np.array(b, dtype=np.uint8)
+        
+        x.extend(a)
+        y.extend(b)
+
+    #return tf.convert_to_tensor(x), tf.convert_to_tensor(y)
+    return np.array(x), np.array(y)
+
+
+# Image Creation
+def creation_input_model(folder_path, multitask, binarization, batch_size, target_size_1, target_size_2):
+
+    datagenerator=ImageDataGenerator(preprocessing_function=preprocess_input)
+
+    if not binarization:
+        columns = ['peak', 'playground', 'train_station', 'metro_station', 'tram_stop', 'bus_stop', \
+            'university', 'parking_car', 'parking_bicycle', 'parking_motorcycle', 'water_natural', \
+            'water_artificial', 'park', 'grassland', 'farmland', 'aerodrome', 'highway_residential', \
+            'highway_cycleway', 'highway_pedestrian', 'highway_less', 'highway_some', 'highway_more', \
+            'building_less', 'building_some', 'building_more']
+        n_col = 1
+        offset = 19
+    else:
+        columns = ['peak_yes', 'peak_no', 'playground_yes', 'playground_no', 'train_station_yes', \
+            'train_station_no', 'metro_station_yes', 'metro_station_no', 'tram_stop_yes', \
+            'tram_stop_no', 'bus_stop_yes', 'bus_stop_no', 'university_yes', 'university_no', \
+            'parking_car_yes', 'parking_car_no', 'parking_bicycle_yes', 'parking_bicycle_no', \
+            'parking_motorcycle_yes', 'parking_motorcycle_no', 'water_natural_yes', 'water_natural_no', \
+            'water_artificial_yes', 'water_artificial_no', 'park_yes', 'park_no', 'grassland_yes', \
+            'grassland_no', 'farmland_yes', 'farmland_no', 'aerodrome_yes', 'aerodrome_no', \
+            'highway_residential_yes', 'highway_residential_no', 'highway_cycleway_yes', \
+            'highway_cycleway_no', 'highway_pedestrian_yes', 'highway_pedestrian_no', \
+            'highway_less', 'highway_some', 'highway_more', \
+            'building_less', 'building_some', 'building_more']
+        n_col = 2
+        offset = 38
+
+    train_metadata_dataframe = pd.read_csv(folder_path + '/grid-creation/data/city_merged/marker_metadata_binarized.csv')
+    train_image_path = folder_path + '/grid-creation/data/city_merged/map_tiles/'
+
+    if not multitask:
+        train_data = generate_data(datagenerator, train_metadata_dataframe, train_image_path, \
+                                                    columns, batch_size, target_size_1, target_size_2)
+    else:
+        train_x, train_y = generate_data_multitasking(datagenerator, \
+                                                    train_metadata_dataframe, train_image_path, \
+                                                    columns, batch_size, target_size_1, target_size_2)
+        print(train_x.shape)
+        print(train_y.shape)
+
+        train_targets = [[train_y[i][c*n_col:(c+1)*n_col] for i in range(train_y.shape[0])] for c in range(19)]
+        train_targets.append([train_y[i][offset+0*3:offset+(0+1)*3] for i in range(train_y.shape[0])])
+        train_targets.append([train_y[i][offset+1*3:offset+(1+1)*3] for i in range(train_y.shape[0])])
+        train_targets = [np.array(e) for e in train_targets]
+        #train_targets = [tf.convert_to_tensor(e) for e in train_targets]
+
+        #print(len(train_targets))
+        #for prova in train_targets:
+        #    print(prova)
+        #    print(prova.shape)
+
+        train_data = (train_x, train_targets)
+
+
+    validation_metadata_dataframe = pd.read_csv(folder_path + '/grid-creation/data/milano_merged/marker_metadata_binarized.csv')
+    validation_image_path = folder_path + '/grid-creation/data/milano_merged/map_tiles/'
+    
+    if not multitask:
+        validation_data = generate_data(datagenerator, validation_metadata_dataframe, validation_image_path, \
+                                                    columns, batch_size, target_size_1, target_size_2)
+    else:
+        validation_x, validation_y = generate_data_multitasking(datagenerator, \
+                                                    validation_metadata_dataframe, validation_image_path, \
+                                                    columns, batch_size, target_size_1, target_size_2)
+        print(validation_x.shape)
+        print(validation_y.shape)
+
+        validation_targets = [[validation_y[i][c*n_col:(c+1)*n_col] for i in range(validation_y.shape[0])] for c in range(19)]
+        validation_targets.append([validation_y[i][offset+0*3:offset+(0+1)*3] for i in range(validation_y.shape[0])])
+        validation_targets.append([validation_y[i][offset+1*3:offset+(1+1)*3] for i in range(validation_y.shape[0])])
+        validation_targets = [np.array(e) for e in validation_targets]
+        #validation_targets = [tf.convert_to_tensor(e) for e in validation_targets]
+    
+        validation_data = (validation_x, validation_targets)
+
+    return train_data, validation_data
--- map-embedding/main.py
+++ map-embedding/main.py
@@ -1,4 +1,5 @@
 from model import *
+from input_generator import *
 import pickle
 import numpy as np
 import pandas as pd
@@ -52,13 +53,20 @@ parser.set_defaults(multitasking=False)
 parser.add_argument('--binarization', help="Use dataframe column with binarization", \
                                                 dest='binarization', action='store_true')
 parser.set_defaults(binarization=False)
+parser.add_argument('--load_input', help="Load input preprocessed", \
+                                                dest='load_input', action='store_true')
+parser.set_defaults(load_input=False)
 args = parser.parse_args()
 
+if args.load_input and not args.multitasking:
+    print('Error on use --load_input without --multitasking')
+    exit(1)
 if args.binarization and not args.multitasking:
     print('Error on use --binarization without --multitasking')
     exit(1)
 multitask = args.multitasking
 binarization = args.binarization
+load_input = args.load_input
 
 
 if __name__ == '__main__':
@@ -76,7 +84,7 @@ if __name__ == '__main__':
     os.chdir(os.getcwd())
 
     folder_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    nb_epoch = 20  # number of epoch at training stage
+    nb_epoch = 1  # number of epoch at training stage
     batch_size = 64 # number of batch at training stage
     vector_shape = 128 # dimension of the embedding vector
     target_size_1 = 224
@@ -101,108 +109,22 @@ if __name__ == '__main__':
 
     # Image Creation
 
-    datagenerator=ImageDataGenerator(preprocessing_function=preprocess_input)
-
-    if not binarization:
-        columns = ['peak', 'playground', 'train_station', 'metro_station', 'tram_stop', 'bus_stop', \
-            'university', 'parking_car', 'parking_bicycle', 'parking_motorcycle', 'water_natural', \
-            'water_artificial', 'park', 'grassland', 'farmland', 'aerodrome', 'highway_residential', \
-            'highway_cycleway', 'highway_pedestrian', 'highway_less', 'highway_some', 'highway_more', \
-            'building_less', 'building_some', 'building_more']
-        n_col = 1
-        offset = 19
-    else:
-        columns = ['peak_yes', 'peak_no', 'playground_yes', 'playground_no', 'train_station_yes', \
-            'train_station_no', 'metro_station_yes', 'metro_station_no', 'tram_stop_yes', \
-            'tram_stop_no', 'bus_stop_yes', 'bus_stop_no', 'university_yes', 'university_no', \
-            'parking_car_yes', 'parking_car_no', 'parking_bicycle_yes', 'parking_bicycle_no', \
-            'parking_motorcycle_yes', 'parking_motorcycle_no', 'water_natural_yes', 'water_natural_no', \
-            'water_artificial_yes', 'water_artificial_no', 'park_yes', 'park_no', 'grassland_yes', \
-            'grassland_no', 'farmland_yes', 'farmland_no', 'aerodrome_yes', 'aerodrome_no', \
-            'highway_residential_yes', 'highway_residential_no', 'highway_cycleway_yes', \
-            'highway_cycleway_no', 'highway_pedestrian_yes', 'highway_pedestrian_no', \
-            'highway_less', 'highway_some', 'highway_more', \
-            'building_less', 'building_some', 'building_more']
-        n_col = 2
-        offset = 38
-
-    train_metadata_dataframe = pd.read_csv(folder_path + '/grid-creation/data/city_merged/marker_metadata_binarized.csv')
-    train_image_path = folder_path + '/grid-creation/data/city_merged/map_tiles/'
-
-    train_x, train_y = generate_data_from_directory(datagenerator, \
-                                                    train_metadata_dataframe, train_image_path, \
-                                                    columns, batch_size, target_size_1, target_size_2)
-
-    if not multitask:
-        train_targets = train_y
-    else:
-        print(train_x.shape)
-        print(train_y.shape)
-
-        train_targets = [[train_y[i][c*n_col:(c+1)*n_col] for i in range(train_y.shape[0])] for c in range(19)]
-        train_targets.append([train_y[i][offset+0*3:offset+(0+1)*3] for i in range(train_y.shape[0])])
-        train_targets.append([train_y[i][offset+1*3:offset+(1+1)*3] for i in range(train_y.shape[0])])
-        train_targets = [np.array(e) for e in train_targets]
-        #train_targets = [tf.convert_to_tensor(e) for e in train_targets]
-
-        """    
-        train_targets = []
-        for i in range(train_y.shape[0]):
-            if not train_targets:
-                for c in range(19):
-                    #print(c*n_col, (c+1)*n_col)
-                    train_targets.append([train_y[i][c*n_col:(c+1)*n_col]])
-                for c in range(2):
-                    #print(offset+3*c, offset+3*(c+1))
-                    train_targets.append([train_y[i][offset+c*3:offset+(c+1)*3]])
-            else:
-                for c in range(19):
-                    train_targets[c].append(train_y[i][c*n_col:(c+1)*n_col])
-                for c in range(2):
-                    train_targets[19+c].append(train_y[i][offset+c*3:offset+(c+1)*3])
-        
-        train_targets = [np.array(e) for e in train_targets]
-        """
-
-        #print(len(train_targets))
-        #for prova in train_targets:
-        #    print(prova)
-        #    print(prova.shape)
-
-
-    validation_metadata_dataframe = pd.read_csv(folder_path + '/grid-creation/data/milano_merged/marker_metadata_binarized.csv')
-    validation_image_path = folder_path + '/grid-creation/data/milano_merged/map_tiles/'
+    if not load_input:
+        train_data, validation_data = creation_input_model(folder_path, multitask, binarization, \
+                                                        batch_size, target_size_1, target_size_2)
+        if isinstance(train_data, tuple):
+            train_x, train_targets = train_data
+            validation_x, validation_targets = validation_data
+            with open(os.path.join('results', "train_x.pickle"),'wb') as f: pickle.dump(train_x, f)
+            with open(os.path.join('results', "train_targets.pickle"),'wb') as f: pickle.dump(train_targets, f)
+            with open(os.path.join('results', "validation_x.pickle"),'wb') as f: pickle.dump(validation_x, f)
+            with open(os.path.join('results', "validation_targets.pickle"),'wb') as f: pickle.dump(validation_targets, f)
     
-    validation_x, validation_y = generate_data_from_directory(datagenerator, \
-                                                validation_metadata_dataframe, validation_image_path, \
-                                                columns, batch_size, target_size_1, target_size_2)
-    if not multitask:
-        validation_targets = validation_y
     else:
-        print(validation_x.shape)
-        print(validation_y.shape)
-
-        validation_targets = [[validation_y[i][c*n_col:(c+1)*n_col] for i in range(validation_y.shape[0])] for c in range(19)]
-        validation_targets.append([validation_y[i][offset+0*3:offset+(0+1)*3] for i in range(validation_y.shape[0])])
-        validation_targets.append([validation_y[i][offset+1*3:offset+(1+1)*3] for i in range(validation_y.shape[0])])
-        validation_targets = [np.array(e) for e in validation_targets]
-        #validation_targets = [tf.convert_to_tensor(e) for e in validation_targets]
-
-        """
-        validation_targets = []
-        for i in range(validation_y.shape[0]):
-            if not validation_targets:
-                for c in range(19):
-                    validation_targets.append([validation_y[i][c*n_col:(c+1)*n_col]])
-                for c in range(2):
-                    validation_targets.append([validation_y[i][offset+c*3:offset+(c+1)*3]])
-            else:
-                for c in range(19):
-                    validation_targets[c].append(validation_y[i][c*n_col:(c+1)*n_col])
-                for c in range(2):
-                    validation_targets[19+c].append(validation_y[i][offset+c*3:offset+(c+1)*3])
-        validation_targets = [np.array(e) for e in validation_targets]
-        """
+        train_x = pickle.load(open(os.path.join('results', "train_x.pickle"),'rb'))
+        train_targets = pickle.load(open(os.path.join('results', "train_targets.pickle"),'rb'))
+        validation_x = pickle.load(open(os.path.join('results', "validation_x.pickle"),'rb'))
+        validation_targets = pickle.load(open(os.path.join('results', "validation_targets.pickle"),'rb'))
 
 
     # Model creation
@@ -236,12 +158,22 @@ if __name__ == '__main__':
 
     print("training model...")
     ts = time.time()
-    history = build.fit(train_x, train_targets,
-                        epochs=nb_epoch,
-                        batch_size=batch_size,
-                        validation_data=(validation_x, validation_targets),
-                        callbacks=[model_checkpoint],
-                        verbose=1)
+
+    if not multitask:
+        history = build.fit(train_data,
+                            epochs=nb_epoch,
+                            batch_size=batch_size,
+                            validation_data=validation_data,
+                            callbacks=[model_checkpoint],
+                            verbose=1)
+    else:
+        history = build.fit(train_x, train_targets,
+                            epochs=nb_epoch,
+                            batch_size=batch_size,
+                            validation_data=(validation_x, validation_targets),
+                            callbacks=[model_checkpoint],
+                            verbose=1)
+
     build.save_weights(os.path.join(
         'MODEL', '{}.h5'.format(hyperparams_name)), overwrite=True)
     pickle.dump((history.history), open(os.path.join(
@@ -256,6 +188,7 @@ if __name__ == '__main__':
 
 
     # Prediction
+    datagenerator=ImageDataGenerator(preprocessing_function=preprocess_input)
     test_metadata_dataframe = pd.read_csv(folder_path + '/grid-creation/data/milano_merged/marker_metadata_binarized.csv')
     test_image_path = folder_path + '/grid-creation/data/milano_merged/map_tiles/'
 
